217c49600dc52793771cf93d46e20d73506fcf91
Davin <85967571+dav1n9@users.noreply.github.com>
2024-11-14 15:12:08 +0900

    feat: set up the project

---
+++ src/main/java/com/example/banthing/BanThingApplication.java
@@ -0,0 +1,13 @@
+package com.example.banthing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanThingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanThingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/banthing/BanThingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banthing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanThingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
