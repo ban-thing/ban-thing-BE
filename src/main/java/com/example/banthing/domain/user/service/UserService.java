e9d5a0be784e4b5251b82abf8202085235309dcc
Davin <85967571+dav1n9@users.noreply.github.com>
2024-12-01 16:58:55 +0900

    feat: 구매목록 조회 기능 구현

--- src/main/java/com/example/banthing/domain/user/service/UserService.java
+++ src/main/java/com/example/banthing/domain/user/service/UserService.java
@@ -1,10 +1,13 @@
 package com.example.banthing.domain.user.service;
 
 import com.example.banthing.domain.user.dto.ProfileResponseDto;
+import com.example.banthing.domain.user.dto.PurchaseResponseDto;
 import com.example.banthing.domain.user.entity.User;
 import com.example.banthing.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,10 +15,17 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public ProfileResponseDto findById(Long userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new NullPointerException("해당 유저는 존재하지 않습니다."));
+    public ProfileResponseDto findMyProfile (Long userId) {
+        return new ProfileResponseDto(findById(userId));
+    }
 
-        return new ProfileResponseDto(user);
+    public List<PurchaseResponseDto> findPurchasesById(Long userId) {
+        User user = findById(userId);
+        return user.getPurchases().stream().map(PurchaseResponseDto::new).toList();
+    }
+
+    private User findById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new NullPointerException("해당 유저는 존재하지 않습니다."));
     }
 }
