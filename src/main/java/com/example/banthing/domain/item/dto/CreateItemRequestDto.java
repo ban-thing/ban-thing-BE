04978ed93e1fdd61cd6d2d662ed99e13747a4e16
Jeahyuk Jeong <jeong_j1@denison.edu>
2024-12-21 10:50:51 +0900

    [fix] 상품 등록 수정

--- src/main/java/com/example/banthing/domain/item/dto/CreateItemRequestDto.java
+++ src/main/java/com/example/banthing/domain/item/dto/CreateItemRequestDto.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.example.banthing.domain.item.serialization.MultipartFileSerializer;
 
@@ -34,7 +36,7 @@ public class CreateItemRequestDto {
     private List<String> hashtags;
 
     // 이미지 파일
-    @JsonSerialize(using = MultipartFileSerializer.class)
-    // @JsonIgnore
+    //@JsonSerialize(using = MultipartFileSerializer.class)
+    @JsonIgnore
     private List<MultipartFile> images;
 }
