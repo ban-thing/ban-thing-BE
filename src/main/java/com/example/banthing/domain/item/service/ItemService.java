4adea0314a411471ba385521af4975ae42ff5144
Jeahyuk Jeong <jeong_j1@denison.edu>
2024-12-21 15:53:30 +0900

    [fix] 상품 등록 수정

--- src/main/java/com/example/banthing/domain/item/service/ItemService.java
+++ src/main/java/com/example/banthing/domain/item/service/ItemService.java
@@ -56,7 +56,7 @@ public class ItemService {
     private final HashtagService hashtagService;
     private final HashtagRepository hashtagRepository;
     
-    public ItemResponseDto save(Long id, CreateItemRequestDto request) throws IOException {
+    public void save(Long id, CreateItemRequestDto request) throws IOException {
         //logger.info("cleaning detail in Service: {}", objectMapper.writeValueAsString(request));
         //MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
         //User seller = userRepository.findById(id).orElseThrow(NullPointerException::new);
@@ -91,7 +91,7 @@ public class ItemService {
         hashtagService.save(request.getHashtags(), item.getId());
         itemImgsService.save(request.getImages(), item.getId());
          */
-        return new ItemResponseDto(null);
+        //return new ItemResponseDto(item);
         
     }
 
