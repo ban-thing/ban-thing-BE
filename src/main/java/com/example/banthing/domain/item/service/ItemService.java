a3f365e44a6cf3932694854ea4bfa6e1269d9528
Jeahyuk Jeong <jeong_j1@denison.edu>
2024-12-21 17:09:09 +0900

    [fix] 상품 등록 수정

--- src/main/java/com/example/banthing/domain/item/service/ItemService.java
+++ src/main/java/com/example/banthing/domain/item/service/ItemService.java
@@ -89,7 +89,7 @@ public class ItemService {
                 .build());
         
         hashtagService.save(request.getHashtags(), item.getId());
-        itemImgsService.save(request.getImages(), item.getId());
+        itemImgsService.save(images, item.getId());
         
         return new ItemResponseDto(null);
         
