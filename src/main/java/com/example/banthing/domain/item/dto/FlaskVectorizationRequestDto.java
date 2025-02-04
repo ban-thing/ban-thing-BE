d30e02392e07c4fa48c6837b8864a802bda43f22
Jeahyuk Jeong <jeong_j1@denison.edu>
2025-02-03 22:15:01 -0500

    [update]

--- src/main/java/com/example/banthing/domain/item/dto/FlaskVectorizationRequestDto.java
+++ src/main/java/com/example/banthing/domain/item/dto/FlaskVectorizationRequestDto.java
@@ -2,7 +2,12 @@ package com.example.banthing.domain.item.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class FlaskVectorizationRequestDto {
+    @JsonProperty("input_hashtag")
     private String input_hashtag;
 
     // Constructors
