9491ae426e5b8e4d9c0c17ae1835b01ecfe0d614
Jeahyuk Jeong <jeong_j1@denison.edu>
2025-02-16 08:33:06 -0500

    [update]

--- src/main/java/com/example/banthing/domain/item/dto/FlaskRequestDto.java
+++ src/main/java/com/example/banthing/domain/item/dto/FlaskRequestDto.java
@@ -2,14 +2,11 @@ package com.example.banthing.domain.item.dto;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.Getter;
 
 @Getter
 public class FlaskRequestDto {
-    
-    @JsonProperty("input_hashtag")
+
     private String input_hashtag;
     List<ItemSearchResponseDto> items;
 
