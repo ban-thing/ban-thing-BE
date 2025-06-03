8c5a73611bf1a73b4702ccb6931e40e199ee82fb
Jeahyuk Jeong <jeong_j1@denison.edu>
2025-06-03 18:14:39 +0900

    [UPDATE]

--- src/main/java/com/example/banthing/domain/chat/websocket/WebSocketConfig.java
+++ src/main/java/com/example/banthing/domain/chat/websocket/WebSocketConfig.java
@@ -1,11 +1,14 @@
 package com.example.banthing.domain.chat.websocket;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
  
 @Configuration
 @EnableWebSocket
@@ -21,5 +24,13 @@ public class WebSocketConfig implements WebSocketConfigurer {
                 .addHandler(webSocketHandler, "/ws/chat/{roomId}")
                 // CORS 허용
                 .setAllowedOrigins("*");
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(1024 * 1024);
+        container.setMaxBinaryMessageBufferSize(1024 * 1024);
+        return container;
     }
 }
